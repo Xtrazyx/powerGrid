1cd6c6e6fb4572cf217ad6b44fe1f002647b2e8a
xtrazyx <xtrazyx@gmail.com>
2020-06-14 21:49:34 +0200

    change in app

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
-import {PowerGrid} from "./powerGrid";
+import { PowerGrid } from "./powerGrid";
 
 export default function App(){
-  const data = {
-      hello: 'Hello'
-  };
-
   return (
     <div>
-      <PowerGrid data={data}/>
+      <PowerGrid />
     </div>)
 }
